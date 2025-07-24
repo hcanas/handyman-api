fbad9b7379824e63986ceee19dc7665327a89db7
hcanas <harveyanas@gmail.com>
2025-07-24 19:22:41 +0800

    add tests for unbanning users

---
+++ tests/Feature/Admin/UnbanUserTest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Models\User;
+use App\UserRole;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class UnbanUserTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    protected User $auth_user;
+
+    protected User $target_user;
+
+    protected string $token;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->auth_user = User::factory()->create(['role' => UserRole::Admin->value]);
+        $this->token = $this->auth_user->createToken('web')->plainTextToken;
+        Sanctum::actingAs($this->auth_user);
+
+        $this->target_user = User::factory()->create([
+            'role' => UserRole::Staff->value,
+            'banned_at' => now()->subMinutes(5),
+            'ban_reason' => $this->faker->sentence(),
+        ]);
+    }
+
+    public function test_can_unban_if_authenticated_user_is_an_admin(): void
+    {
+        $response = $this
+            ->withCookie('token', $this->token)
+            ->patchJson(route('unban-user'));
+
+        $this->assertTrue($this->auth_user->isAdmin());
+        $response->assertUnprocessable();
+    }
+
+    public function test_cannot_unban_if_authenticated_user_is_not_an_admin(): void
+    {
+        $this->auth_user->update(['role' => UserRole::Staff->value]);
+
+        $response = $this
+            ->withCookie('token', $this->token)
+            ->patchJson(route('unban-user'));
+
+        $this->assertFalse($this->auth_user->isAdmin());
+        $response->assertForbidden();
+    }
+
+    public function test_succeeds_if_target_user_is_banned(): void
+    {
+        $response = $this
+            ->withCookie('token', $this->token)
+            ->patchJson(route('unban-user'), [
+                'user_id' => $this->target_user->id,
+            ]);
+
+        $this->assertTrue($this->target_user->isBanned());
+        $response->assertOk();
+    }
+
+    public function test_fails_if_target_user_is_active(): void
+    {
+        $this->target_user->update([
+            'banned_at' => null,
+            'ban_reason' => null,
+        ]);
+
+        $response = $this
+            ->withCookie('token', $this->token)
+            ->patchJson(route('unban-user'), [
+                'user_id' => $this->target_user->id,
+            ]);
+
+        $this->assertFalse($this->target_user->isBanned());
+        $response->assertConflict();
+    }
+
+    public function test_fails_if_input_is_empty(): void
+    {
+        $response = $this
+            ->withCookie('token', $this->token)
+            ->patchJson(route('unban-user'));
+
+        $response->assertUnprocessable();
+        $response->assertJsonValidationErrors(['user_id']);
+    }
+
+    public function test_fails_if_target_user_is_not_registered(): void
+    {
+        $user_id = 999;
+
+        $response = $this
+            ->withCookie('token', $this->token)
+            ->patchJson(route('unban-user'), ['user_id' => $user_id]);
+
+        $this->assertDatabaseMissing('users', ['user_id' => $user_id]);
+        $response->assertUnprocessable();
+        $response->assertJsonValidationErrors(['user_id']);
+    }
+}
