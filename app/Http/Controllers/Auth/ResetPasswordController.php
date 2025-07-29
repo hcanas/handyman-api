0dfa935a2623f39db6951e51d8b9d351ce56daf9
hcanas <harveyanas@gmail.com>
2025-07-29 19:43:15 +0800

    delete corresponding otp on password change

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,22 +4,41 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\ResetPasswordRequest;
+use App\Models\PasswordOtp;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class ResetPasswordController extends Controller
 {
     public function __invoke(ResetPasswordRequest $request)
     {
-        $user = User::query()
-            ->where('email', $request->validated('email'))
-            ->first();
+        try {
+            DB::beginTransaction();
 
-        $user?->update(['password' => Hash::make($request->password)]);
+            User::query()
+                ->where('email', $request->validated('email'))
+                ->update(['password' => Hash::make($request->password)]);
 
-        // do not return an error if user doesn't exist for security purposes
-        return response()->json([
-            'message' => 'New password has been saved',
-        ]);
+            PasswordOtp::query()
+                ->where('email', $request->validated('email'))
+                ->where('otp', $request->validated('otp'))
+                ->delete();
+
+            DB::commit();
+
+            // do not return an error if user doesn't exist for security purposes
+            return response()->json([
+                'message' => 'New password has been saved',
+            ]);
+        } catch (\Throwable $e) {
+            DB::rollBack();
+
+            logger($e);
+
+            return response()->json([
+                'message' => 'An unexpected error occurred',
+            ], 500);
+        }
     }
 }
