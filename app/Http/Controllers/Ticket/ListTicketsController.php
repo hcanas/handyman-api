1ad51b410e15ce6ff9284068afd80239b32d3ebc
hcanas <harveyanas@gmail.com>
2025-08-16 16:24:16 +0800

    add conditions for keyword search, add query string for returned pagination links

--- app/Http/Controllers/Ticket/ListTicketsController.php
+++ app/Http/Controllers/Ticket/ListTicketsController.php
@@ -24,15 +24,24 @@ class ListTicketsController extends Controller
         $tickets = Cache::tags($cache_tags)
             ->remember($cache_key, $cache_ttl, function () use ($request, $user) {
                 $query = Ticket::query()
+                    ->where(function ($query) use ($request) {
+                        $query->where('id', $request->validated('keyword'))
+                            ->orWhere('title', 'LIKE', '%' . $request->validated('keyword') . "%")
+                            ->orWhere('description', 'LIKE', '%' . $request->validated('keyword') . "%")
+                            ->orWhere('status', $request->validated('keyword'))
+                            ->orWhere('priority_level', $request->validated('keyword'));
+                    })
                     ->with(['reporter', 'assignee']);
 
                 if (!$user->isAdmin()) {
-                    $query = $query->where('reported_by_id', $user->id)
-                        ->orWhere('assigned_to_id', $user->id)
-                        ->orWhereHas('logs', function ($logs_query) use ($user) {
-                            $logs_query
-                                ->where('action', TicketAction::ReceivedAssignment->value)
-                                ->where('user_id', $user->id);
+                    $query = $query->where(function ($query) use ($user) {
+                        $query->where('reported_by_id', $user->id)
+                            ->orWhere('assigned_to_id', $user->id)
+                            ->orWhereHas('logs', function ($logs_query) use ($user) {
+                                $logs_query
+                                    ->where('action', TicketAction::ReceivedAssignment->value)
+                                    ->where('user_id', $user->id);
+                            });
                         });
                 }
 
@@ -42,6 +51,8 @@ class ListTicketsController extends Controller
                         $request->validated('order_dir', 'desc'),
                     )->paginate($request->validated('per_page', 15));
             });
+
+        $tickets->withQueryString();
 
         return TicketResource::collection($tickets);
     }
